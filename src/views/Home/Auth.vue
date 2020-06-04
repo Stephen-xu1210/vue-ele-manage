5269b2c0e6acf889f2eb301dc63a65e529c8f3af
Stephen.xu <435471845@qq.com>
2020-06-04 20:28:45 +0800

    '权限管理完成'

--- src/views/Home/Auth.vue
+++ src/views/Home/Auth.vue
@@ -1,15 +1,53 @@
 <template>
     <div>
-        <h2>权限列表页</h2>
+        <!-- 面包屑组件 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/' }">权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限等级列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+        <el-table :data="tableData" stripe border >
+          <el-table-column label="#" type="index"></el-table-column>
+          <el-table-column prop="authName" label="权限名称"></el-table-column>
+          <el-table-column prop="path" label="路径"></el-table-column>
+          <el-table-column prop="level" label="权限等级">
+              <template v-slot="scope">
+                  <el-tag v-if="scope.row.level === '0'">一级</el-tag>
+                  <el-tag type="warning" v-else-if="scope.row.level === '1'">二级</el-tag>
+                  <el-tag type="danger" v-else>三级</el-tag>
+              </template>
+          </el-table-column>
+        </el-table>
+    </el-card>
+    
     </div>
 </template>
 
 <script>
 export default {
-    name:'Auth'
+    name:'Auth',
+    data() {
+        return {
+            tableData:[]
+        }
+    },
+    mounted() {
+        this.getAuthInfo()
+    },
+    methods: {
+        // 获取权限列表数据
+        async getAuthInfo(){
+         const res = await this.$axios.get('auths/list')
+         console.log('auth res ',res);
+         this.tableData = res.data;
+        }
+    },
 }
 </script>
 
 <style lang="less" scoped>
-
+.el-breadcrumb {
+  margin-bottom: 15px;
+}
 </style>
--- src/views/Home/Roles.vue
+++ src/views/Home/Roles.vue
@@ -1,19 +1,67 @@
 <template>
-    <div>
+  <div>
+    <!-- 面包屑组件 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/' }">权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片组件 -->
+    <el-card class="box-card">
+      <el-row>
+        <el-col :span="2">
+          <el-button type="primary">新增角色</el-button>
+        </el-col>
+      </el-row>
 
-        <h2>角色列表页面</h2>
-        
-    </div>
+      <el-table :data="tableData" style="width: 100%;margin-top:15px;" stripe  border >
+        <el-table-column type="expand">
+          <template v-slot="scope">
+            <pre>
+              {{scope.row}}
+            </pre>
+          </template>
+        </el-table-column>
+        <el-table-column label="#" type="index"></el-table-column>
+        <el-table-column prop="roleName" label="角色名称"></el-table-column>
+        <el-table-column prop="roleDetail" label="角色描述"></el-table-column>
+        <el-table-column label="操作" width="300">
+          <template v-slot="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini">修改</el-button>
 
-    
+            <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+
+            <el-button type="warning" icon="el-icon-user-solid" size="mini">分配角色</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'Roles'
-}
+  name: "Roles",
+  data() {
+    return {
+      tableData: []
+    };
+  },
+  mounted() {
+    this.getRolesInfo();
+  },
+  methods: {
+    async getRolesInfo(){
+      const res  =  await this.$axios.get('/auths');
+      console.log('roles res',res);
+      this.tableData = res.data;
+    }
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
+.el-breadcrumb {
+  margin-bottom: 15px;
+}
 </style>
